6c623703ebc92e79f9957335439b856df002b17a
Diego <diego444x@gmail.com>
2021-03-10 15:08:14 -0300

    init

---
+++ app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="App">
+    <div class="form">
+        <input id="txt" type="text" class="form-control" placeholder="Pokemon" aria-label="Username" aria-describedby="addon-wrapping">
+        <button type="button" class="btn btn-warning">Send</button>
+    </div>
+    <Card></Card>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card'
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+<style scoped>
+.App{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+#txt{
+  margin-right: 5px;
+}
+.form{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 10px;
+}
+</style>
+
---
+++ app/src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="#" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nome: </h5>
+        <p class="card-text">Class:</p>
+        <p class="card-text">Abilidade:</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'Card'
+}
+</script>
