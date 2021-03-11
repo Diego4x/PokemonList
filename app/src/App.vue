c10d13c0f0a934743412d15dc9c52bbcddf0b7fa
Diego <diego444x@gmail.com>
2021-03-11 17:16:59 -0300

    funciona

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,23 +4,26 @@
         <input v-model="text" id="txt" type="text" class="form-control" placeholder="Pokemon" aria-label="Username" aria-describedby="addon-wrapping">
         <button type="button" class="btn btn-warning" @click="getData()">Send</button>
     </div>
-    <Card 
-      :name = "dados.name"
-      :id = "dados.id"
-     />
+   <div class="card" style="width: 18rem;">
+  <img :src="userProfilePic"  class="rounded mx-auto d-block"  alt="..." style="width: 85px">
+  <div class="card-body">
+  <ul class="list-group list-group-flush">
+     <li class="list-group-item">Name: {{dados.name}}</li>
+    <li class="list-group-item">Type: {{type}}</li>
+  </ul>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
-import Card from './components/Card'
 export default {
   name: 'App',
-  components: {
-    Card
-  },
   data(){
     return {
           dados : [],
+          userProfilePic: "",
+          type: "",
           text: ""
     }
   },
@@ -33,6 +36,8 @@ export default {
     .then(response => response.json())
     .then(data => {
       this.dados = data
+      this.type = data.types[0].type.name
+      this.userProfilePic = `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`
     })
     .catch(err => console.log(err));
     }
